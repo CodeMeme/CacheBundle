3f7ff7f6c122ad556eae9c0b883cd8a3bcace431
Eric Clemmons <eric@smarterspam.com>
2011-06-20 09:07:01 -0700

    Added DependencyInjection extension

---
+++ DependencyInjection/CodeMemeCacheExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CodeMeme\CacheBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class CodeMemeCacheExtension extends Extension
+{
+
+    public function load(Array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('cache.xml');
+    }
+
+}
